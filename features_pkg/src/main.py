460ffc2a7bf341ef5faed6f8dffdca072e482756
Gabriela <gabassegio@gmail.com>
2023-06-20 23:05:05 -0300

    Feature ready for testing

--- features_pkg/src/main.py
+++ features_pkg/src/main.py
@@ -8,51 +8,60 @@ from tensorflow.keras.models import load_model
 from PIL import Image, ImageFile
 from u2net_test import mask
 from features_pkg.srv import features
-from data_loader import RescaleT
-from data_loader import ToTensor
-from data_loader import ToTensorLab
-from data_loader import SalObjDataset
-from model import U2NETP # small version u2net 4.7 MB
 from sensor_msgs.msg import Image as imgmsg
 from cv_bridge import CvBridge
+from colorthief import ColorThief
+import webcolors
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import cv2
-import extcolors
 import os
 import argparse
 import glob
 import json
+import extcolors
 import rospy
 import time
+import dlib
+import statistics
 
-
+BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+            "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+            "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+            "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
+POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+            ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+            ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+            ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+            ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
 class bonusFeatures:
 
     def __init__(self):
-        rospy.Service('features', features, self.handler)
         self.point = None
         self.cint = None
         self.knee = None
         self.neck = None 
-        self.BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-                    "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-                    "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-                    "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
-        self.POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-                    ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-                    ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-                    ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-                    ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
         self.topic = "/usb_cam/image_raw"
+        self.rate = rospy.Rate(5)
+        rospy.Service('features', features, self.handler)
         self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
 
     def body_points(self,frame):
+        inWidth = 480
+        inHeight = 640
+
+        net = cv2.dnn.readNetFromTensorflow("/home/bibo/catkin_dev/src/hera_face/features_pkg/src/graph_opt.pb")
+
+        net.setInput(cv2.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+        out = net.forward()
+        out = out[:, :19, :, :] 
+        assert (len(BODY_PARTS) == out.shape[1]) 
+
         points = []
         frameWidth = frame.shape[1]
         frameHeight = frame.shape[0]
-        for i in range(len(self.BODY_PARTS)): 
+        for i in range(len(BODY_PARTS)): 
             heatMap = out[0, i, :, :]
 
             _, conf, _, point = cv2.minMaxLoc(heatMap)
@@ -60,14 +69,14 @@ class bonusFeatures:
             y = (frameHeight * point[1]) / out.shape[2]
             points.append((int(x), int(y)) if conf > 0.2 else None)
 
-        for pair in self.POSE_PAIRS:
+        for pair in POSE_PAIRS:
             partFrom = pair[0]
             partTo = pair[1]
-            assert (partFrom in self.BODY_PARTS)
-            assert (partTo in self.BODY_PARTS)
+            assert (partFrom in BODY_PARTS)
+            assert (partTo in BODY_PARTS)
 
-            idFrom = self.BODY_PARTS[partFrom]
-            idTo = self.BODY_PARTS[partTo]
+            idFrom = BODY_PARTS[partFrom]
+            idTo = BODY_PARTS[partTo]
 
             if points[idFrom] and points[idTo]:
                 cv2.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
@@ -77,16 +86,15 @@ class bonusFeatures:
             t, _ = net.getPerfProfile()
             freq = cv2.getTickFrequency() / 1000
             lx = cv2.putText(frame, '%.2fms' % (t / freq), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-            cv2.imwrite('results/pose_points.png', lx)
-            with open('points.json', 'w') as f:
+            cv2.imwrite('src/hera_face/features_pkg/src/results/pose_points.png', lx)
+            with open('src/hera_face/features_pkg/src/points.json', 'w') as f:
                 json.dump(points, f)
             break
 
     def creating_mask(self,frame):
-        rospy.loginfo("Starting mask")
         output = mask()
         output = load_img(output)
-        rescale_val = 255
+        rescale_val = 1
         out_img = img_to_array(output) / rescale_val
         THRESHOLD = 0.2
         out_img[out_img > THRESHOLD] = 1
@@ -105,27 +113,14 @@ class bonusFeatures:
         rem_back = (rgba_inp * rgba_out)
         rem_back_scaled = Image.fromarray((rem_back * rescale_val).astype('uint8'), 'RGBA')
 
-        rem_back_scaled.save('results/removed_background.png')
+        rem_back_scaled.save('src/hera_face/features_pkg/src/results/removed_background.png')
         rospy.loginfo("Sucessfully created mask!")
         out_layer = out_img[:,:,1]
         y_starts = [np.where(out_layer.T[i]==1)[0][0] if len(np.where(out_layer.T[i]==1)[0])!=0 else out_layer.T.shape[0]+1 for i in range(out_layer.T.shape[0])]
         
-        global starty
-        starty = min(y_starts)
+        self.starty = min(y_starts)
 
-    def height_estimate(self,distance, height):
-        height = 1280 - height
-        distance += 32
-        camera_image_height = 1.48 * distance
-        if height < 640:
-            subject_height = 640 - height
-            hf = 1.52 - (((subject_height*camera_image_height)/1280)/100)
-        else:
-            subject_height = height - 640
-            hf = (((subject_height*camera_image_height)/1280)/100) + 1.52
-        return hf
-
-    def ifmask(path):
+    def ifmask(self,path):
 	        
         model = load_model('/home/bibo/catkin_dev/src/hera_face/features_pkg/src/mask_detector.model')
         openimage = cv2.imread(path)
@@ -137,37 +132,54 @@ class bonusFeatures:
             return "Mask"
         else:
             return "No Mask"
+    
+    def ifglasses(self,path):
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor('src/hera_face/features_pkg/src/shape_predictor_68_face_landmarks.dat')
+        img = dlib.load_rgb_image(path)
+        rect = detector(img)[0]
+        sp = predictor(img, rect)
+        landmarks = np.array([[p.x, p.y] for p in sp.parts()])
+        nose_bridge_x = []
+        nose_bridge_y = []
+        for i in [28,29,30,31,33,34,35]:
+            nose_bridge_x.append(landmarks[i][0])
+            nose_bridge_y.append(landmarks[i][1])
+                
+        ### x_min and x_max
+        x_min = min(nose_bridge_x)
+        x_max = max(nose_bridge_x)### ymin (from top eyebrow coordinate),  ymax
+        y_min = landmarks[20][1]
+        y_max = landmarks[31][1]
+        img2 = Image.open(path)
+        img2 = img2.crop((x_min,y_min,x_max,y_max))
+
+        img_blur = cv2.GaussianBlur(np.array(img2),(3,3), sigmaX=0, sigmaY=0)
+        edges = cv2.Canny(image =img_blur, threshold1=100, threshold2=200)
+
+        #center strip
+        edges_center = edges.T[(int(len(edges.T)/2))]
+
+        if 255 in edges_center:
+            return 'Glasses'
+        else:
+            return 'No glasses'
 	
     def pose_points(self,frame):
-        rospy.loginfo("finding pose points")
         null = 'null'
         
-        inWidth = 480
-        inHeight = 640
-
-        net = cv2.dnn.readNetFromTensorflow("/home/bibo/catkin_dev/src/hera_face/features_pkg/src/graph_opt.pb")
-
-        net.setInput(
-            cv2.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False)) 
-        out = net.forward()
-        out = out[:, :19, :, :]  
-
-        assert (len(self.BODY_PARTS) == out.shape[1]) 
-        
-        
         self.creating_mask(frame)
-        
 
         self.body_points(frame)
 
-        with open('points.json', 'r') as f:
+        with open('src/hera_face/features_pkg/src/points.json', 'r') as f:
             self.point = json.load(f)
 
         try:
             if self.point[1][1] != null:
                 self.neck = self.point[1][1] 
         except: 
-            self.neck = starty-25
+            self.neck = self.starty-25
 
         try:
             if self.point[12][1] != null:
@@ -175,7 +187,7 @@ class bonusFeatures:
         except:
             try:
                 if self.point[9][1] != null:
-                    self.knee = point[9][1]
+                    self.knee = self.point[9][1]
             except:
                 self.knee = null
         
@@ -187,112 +199,98 @@ class bonusFeatures:
                 if self.point[11][1] != null:
                     self.cint = self.point[11][1]+25
             except:
-                self.cint = ((frame.shape[1]-starty)/12)*7
+                self.cint = int(((frame.shape[1]-self.starty)/12)*7)
 
-        way = glob.glob('images/*')
+        way = glob.glob('src/hera_face/features_pkg/src/images/*')
         for py_file in way:
             try:
                 os.remove(py_file)
             except OSError as e:
-                print(f"Error:{ e.strerror}")
+                rospy.logerr(f"Error:{ e.strerror}")
+  
                 
-        modelo = cv2.imread('results/removed_background.png')
+        modelo = cv2.imread('src/hera_face/features_pkg/src/results/removed_background.png')
 
         for x in range(3):
             if x == 0:
                 foto = modelo[self.neck - 25:self.cint, 0:]
-                cv2.imwrite('images/torso.png', foto)  
+                cv2.imwrite('src/hera_face/features_pkg/src/images/torso.png', foto)  
             elif x == 1:
                 if self.knee != null:
-                    print("KNEE", self.knee)
-                    print("CINT:", self.cint)
                     foto = modelo[self.cint - 25:self.knee, 0:]
-                    cv2.imwrite('images/pernas.png', foto)
+                    cv2.imwrite('src/hera_face/features_pkg/src/images/pernas.png', foto)
                 else: 
                     foto = modelo[self.cint - 25:, 0:]
-                    cv2.imwrite('images/pernas.png', foto)
+                    cv2.imwrite('src/hera_face/features_pkg/src/images/pernas.png', foto)
             else:
                 foto = modelo[:self.neck - 10]
-                cv2.imwrite('images/cabeca.png', foto)    
+                cv2.imwrite('src/hera_face/features_pkg/src/images/cabeca.png', foto)    
 
     def color(self,path):
-        ImageFile.LOAD_TRUNCATED_IMAGES = True 
-        plt.figure(figsize=(9, 9))
-        img = plt.imread(path)
-        plt.imshow(img)
-        plt.axis('off')
-
-        colors_x = extcolors.extract_from_path(img, tolerance=12, limit=12)
-
-        rgb = (colors_x[0][0][0])
-
-        if rgb == (0, 0, 0):
-            rgb = (colors_x[0][1][0])
-        if rgb == (255, 255, 255):
-            return 'White'
-        if rgb < (45, 45, 45):
-            return 'Black'
-        elif rgb[0] == rgb[1] and rgb[1] == rgb[2] and rgb[0] == rgb[2]:
-            return 'Grey'
-
-        elif rgb[0] > rgb[1] and rgb[0] > rgb[2]:
-            if rgb[0] > 209 and rgb[1] > 179 and rgb[2] > 134 and rgb != (255, 192, 203):
-                return 'Beige'
-
-            elif (rgb == (184, 134, 11) or rgb == (189, 83, 107) or rgb == (139, 69, 19) or rgb == (160, 82, 45) or rgb == (
-            188, 143, 143)) or rgb[0] > 204 and rgb[1] > 104 and rgb[2] < 144:
-                return 'Brown'
-
-            elif rgb[0] > 204 and rgb[1] < 193 and rgb[2] > 91:
-                return 'Pink'
-
-            elif rgb == (255, 140, 0) or rgb == (255, 165, 0):
-                return 'Orange'
-
-            elif rgb == (255, 215, 0):
-                return 'Gold'
-            elif rgb == (189, 83, 107):
-                return 'Green'
+        for i in range(2):
+            color = self.findColor(path,i)
+            if 'black' in color:
+                if(self.isitblack(path, i)):  
+                    return 'Black'
             else:
-                return 'Red'
+                return color
 
-        elif rgb[1] > rgb[0] and rgb[1] > rgb[2] or rgb == (47, 79, 79):
-            if rgb == (133, 130, 111) or rgb == (124, 125, 111):
-                return 'Beige'
-            return 'Green'
+    def findColor(self,path,index):
+        color_thief = ColorThief(path)
+        dominant_color = color_thief.get_palette(color_count=3)
+        closest_color = None
+        min_distance = float('inf')
+        for color_name, rgb in webcolors.CSS3_NAMES_TO_HEX.items():
+            css3_rgb = webcolors.hex_to_rgb(rgb)
 
-        elif rgb[2] > rgb[1] and rgb[2] > rgb[0] or rgb == (0, 255, 255) or rgb == (0, 139, 139) or rgb == (0, 128, 128):
-            if rgb[0] > 122 and rgb[1] < 113 and rgb[2] > 203 or rgb == (128, 0, 128) or rgb == (75, 0, 130):
-                return 'Purple'
-            else:
-                return 'Blue'
+            distance = self.delta_e_cie76(dominant_color[index], css3_rgb)
 
-        elif rgb == (128, 128, 0):
-            return 'Green'
-        elif rgb == (255, 255, 0):
-            return 'Yellow'
-        elif rgb == (255, 0, 255) or rgb == (238, 130, 238) or rgb == (218, 112, 214) or rgb == (221, 160, 221):
-            return 'Pink'
+            if distance < min_distance:
+                min_distance = distance
+                closest_color = color_name
+        return closest_color
 
+    def delta_e_cie76(self,color1, color2):
+        r_mean = (color1[0] + color2[0]) / 2
+        delta_r = color1[0] - color2[0]
+        delta_g = color1[1] - color2[1]
+        delta_b = color1[2] - color2[2]
+        return ((2 + (r_mean / 256)) * delta_r ** 2 + 4 * delta_g ** 2 + (2 + ((255 - r_mean) / 256)) * delta_b ** 2) ** 0.5
 
-    def features(self,distance):
+    def isitblack(self,path,index):
+        colors_x, pixel_count = extcolors.extract_from_path(path)
+        pclist = []
+        for color in colors_x:
+            rgb = color[0]
+            count = color[1]
+            percentage = (count / pixel_count) * 100
+            pclist.append(percentage)
+
+        if (pclist[index])>90 :
+            return True
+        else:
+            return False
+
+    
+    def features(self):
         self.bridge = CvBridge()
-        frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='bgr8')
+        frame = self.bridge.imgmsg_to_cv2(self.cam_image,desired_encoding='rgb8')
         time.sleep(1)
-        cv2.imwrite('base/img.png', frame)    
-
+        
+        cv2.imwrite('src/hera_face/features_pkg/src/base/img.png', frame)    
+        
         self.pose_points(frame)
+        body_colors = []
+        for parts in glob.glob('src/hera_face/features_pkg/src/images/*'):
 
-        for parts in glob.glob('images/*'):
-
-            if parts == 'images/cabeca.png':
-                mask = ifmask('images/cabeca.png')
+            if parts == 'src/hera_face/features_pkg/src/images/cabeca.png':
+                mask = self.ifmask('src/hera_face/features_pkg/src/images/cabeca.png')
+                glasses = self.ifglasses('src/hera_face/features_pkg/src/images/cabeca.png')
 
             output_color = self.color(parts)
             body_colors.append(output_color)
-        # Return the list of colors
-        height_estimate(distance, starty)
-        return mask,str(body_colors[0]),str(body_colors[1]), height_estimate
+
+        return mask,glasses,str(body_colors[0]),str(body_colors[1])
 
     def camera_callback(self,data):
         self.cam_image = data
@@ -301,14 +299,14 @@ class bonusFeatures:
         self.recog = 0
         rospy.loginfo("Service called!")
         rospy.loginfo("Requested..")
+        time.sleep(5)
         while self.recog == 0:
             self.img_sub = rospy.Subscriber(self.topic,imgmsg,self.camera_callback)
-            mask, shirt, pants, height = self.features(request)
+            mask, glasses, shirt, pants = self.features()
             self.rate.sleep()
-            return mask, shirt, pants, height
+            return mask, glasses, shirt, pants
         cv2.destroyAllWindows()
     
-
 if __name__ == "__main__":
     rospy.init_node('feature_bonus', log_level=rospy.INFO)
     rospy.loginfo("Service started!")
