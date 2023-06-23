6976a3c529cd4c9eefc3396866b1f36dc40d1520
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2023-06-23 18:08:36 -0300

    First refactor

--- hera_people_recog/src/people.py
+++ hera_people_recog/src/people.py
@@ -30,7 +30,7 @@ class FaceRecog:
         rospy.loginfo("Start camera suscriber...")
         self.topic = "/zed_node/left_raw/image_raw_color"
         self._check_cam_ready()
-        self.image_sub = rospy.Subscriber(self.topic, Image, self._camera_callback)
+        self.image_sub = rospy.Subscriber(self.topic, Image, self.camera_callback)
 
         self.center_place = None
         self.detector = dlib.get_frontal_face_detector()
@@ -55,118 +55,126 @@ class FaceRecog:
             except:
                 rospy.logerr("Current " + self.topic + " not ready yet, retrying.")
 
-    def _camera_callback(self, data):
+    def camera_callback(self, data):
         self.cam_image = data
 
-    def _load_data(self):
+    def load_data(self):
         files = fnmatch.filter(os.listdir(self.people_dir), '*.jpg')
+        self._process_files(files)
 
+    def _process_files(self, files):
         for file_name in files:
             img = dlib.load_rgb_image(os.path.join(self.people_dir, file_name))
-            img_detected = self.detector(img, 1)
-            bounding_boxes = []
+            self._process_image(img, file_name)
 
-            if len(img_detected) > 0:
-                for i, rects in enumerate(img_detected):
-                    area = rects.top() * rects.left()
-                    bounding_boxes.append(area)
-                biggest_box = bounding_boxes.index(max(bounding_boxes))
-                img_shape = self.sp(img, img_detected[biggest_box])
-                align_img = dlib.get_face_chip(img, img_shape)
-                img_rep = np.array(self.model.compute_face_descriptor(align_img))
-                self.known_face.append(img_rep)
-                self.known_name.append(file_name.split('.')[0])
-            else:
-                rospy.loginfo("No face detected in image: " + file_name)
+    def _process_image(self, img, file_name):
+        img_detected = self.detector(img, 1)
+        if not img_detected:
+            rospy.loginfo("No face detected in image: " + file_name)
+            return
+        self._extract_biggest_face(img, img_detected, file_name)
 
-    def _calculate_media(self, x1, x2):
-        return (x1 + x2) / 2
+    def _extract_biggest_face(self, img, img_detected, file_name):
+        bounding_boxes = [rect.width() * rect.height() for rect in img_detected]
+        biggest_box_index = bounding_boxes.index(max(bounding_boxes))
+        biggest_box = img_detected[biggest_box_index]
+        self._align_and_add_face(img, biggest_box, file_name)
 
-    def _spin(self, velocidade=-0.4, sleep_time=0.0):
+    def _align_and_add_face(self, img, box, file_name):
+        img_shape = self.sp(img, box)
+        align_img = dlib.get_face_chip(img, img_shape)
+        img_rep = np.array(self.model.compute_face_descriptor(align_img))
+        self.known_face.append(img_rep)
+        self.known_name.append(file_name.split('.')[0])
+
+    def spin(self, velocidade=-0.4):
         vel_cmd = Twist()
         vel_cmd.angular.z = velocidade
         self.pub_cmd_vel.publish(vel_cmd)
 
-    def _predict(self):
+    def predict(self):
         small_frame = self.bridge_object.imgmsg_to_cv2(self.cam_image, desired_encoding="bgr8")
-        results = self.yolo.predict(source=small_frame, conf=0.3, device=0, classes=[56, 57])
+        results = self.yolo.predict(source=small_frame, conf=0.4, device=0, classes=56)
         print('Len boxes: ', len(results[0].boxes))
         return results[0].boxes
 
-    def _update_boxes(self):
-        return self._predict()
+    def find_sit(self):
+        boxes = self.predict()
 
-    def _find_sit(self):
+        while len(boxes) == 0:
+            print('First while')
+            self.spin(-0.4)
+            boxes = self.predict()
+
+        self.spin(0)
+
         while True:
-            print('single loop')
-            boxes = self._predict()
+            print('Second while')
+            boxes = self.predict()
             if len(boxes) > 0:
                 print('box > 0')
-                self._spin(0, 1)
-                boxes = self._update_boxes()
-                self.center_place = self._find_empty_place(boxes)
+                self.spin(0)
+                time.sleep(1)
+                self.center_place = self.find_empty_place(boxes)
 
             if self.center_place is not None:
                 print('break')
                 break
             else:
-                print('_spin and detect')
-                self._spin(-0.4, 2)
+                print('Spin and detect')
+                self.spin(-0.4)
 
         return self.center_place
 
-    def _find_empty_place(self, boxes):
+    def find_empty_place(self, boxes):
         center_place = None
         print('Looking for an empty place')
-        for k, c in enumerate(boxes.cls):
-            box = boxes[k].xyxy[0]
-            obj_class = self.yolo.names[int(c)]
-            print(obj_class)
-
-            found = False
-            if self.face_center:
-                for i, face_name in enumerate(self.face_name):
-                    if face_name in self.known_name:
-                        found = True
-                        print('box0:', box[0])
-                        print('box2:', box[2])
-                        print('center:', self.face_center[i])
-
-                        if obj_class == 'chair' and not any(box[0] < center_face < box[2] for center_face in self.face_center):
-                            center_place = self._calculate_media(box[0], box[2])
-                            print("lugar 0")
-                        elif obj_class == 'couch':
-                            media_x = self._calculate_media(box[0], box[2])
-                            if not any(box[0] < center_face < media_x for center_face in self.face_center):
-                                center_place = self._calculate_media(box[0], media_x)
-                                print('lugar 1')
-                            elif not any(media_x < center_face < box[2] for center_face in self.face_center):
-                                center_place = self._calculate_media(media_x, box[2])
-                                print('lugar 2')
-
-                if not found:
-                    print('not Found condition')
-                    if obj_class == 'chair':
-                        center_place = self._calculate_media(box[0], box[2])
-                    elif obj_class == 'couch':
-                        media_x = (box[0] + box[2]) / 2
-                        center_place = self._calculate_media(box[0], media_x)
-            else:
-                print('Do not recognized face')
-                if obj_class == 'chair':
-                    center_place = self._calculate_media(box[0], box[2])
-                elif obj_class == 'couch':
-                    media_x = self._calculate_media(box[0], box[2])
-                    center_place = self._calculate_media(box[0], media_x)
-
+        for box in boxes:
+            center_place = self._get_center_place(box, center_place)
         return center_place
 
-    def _recognize(self, small_frame):
+    def _get_center_place(self, box, center_place):
+        obj_class = self.yolo.names[int(box.cls)]
+        print(obj_class)
+        if self.face_center:
+            center_place = self._check_known_faces(box, obj_class, center_place)
+        else:
+            center_place = self._calculate_center_place(box, obj_class)
+        return center_place
+
+    def _check_known_faces(self, box, obj_class, center_place):
+        for i, face_name in enumerate(self.face_name):
+            if face_name in self.known_name:
+                center_place = self._calculate_center_place(box, obj_class, i, face_name)
+            else:
+                center_place = self._calculate_center_place(box, obj_class)
+        return center_place
+
+    def _calculate_center_place(self, box, obj_class, index=None, face_name=None):
+        box = box.xyxy[0]
+        if index is not None and face_name is not None:
+            print('box0:', box[0])
+            print('box2:', box[2])
+            print('center:', self.face_center[index])
+        if obj_class == 'chair' and not any(box[0] < center_face < box[2] for center_face in self.face_center):
+            center_place = (box[0] + box[2]) / 2
+            print("lugar 0")
+        elif obj_class == 'couch':
+            media_x = (box[0] + box[2]) / 2
+            if not any(box[0] < center_face < media_x for center_face in self.face_center):
+                center_place = (box[0] + media_x) / 2
+                print('lugar 1')
+            elif not any(media_x < center_face < box[2] for center_face in self.face_center):
+                center_place = (media_x + box[2]) / 2
+                print('lugar 2')
+        return center_place
+
+    def recognize(self, small_frame):
         img_detected = self.detector(small_frame, 1)
 
         if len(img_detected) == 0:
             rospy.loginfo("No face detected")
-            self._find_sit()
+            self.find_sit()
             return len(img_detected)
 
         faces = dlib.full_object_detections()
@@ -176,25 +184,14 @@ class FaceRecog:
         img_rep = np.array(self.model.compute_face_descriptor(align_img))
 
         for i in range(len(img_detected)):
-            name = 'Face'
-            for j in range(len(self.known_face)):
-                euclidean_dist = list(np.linalg.norm(self.known_face - img_rep[i], axis=1) <= 0.62)
-                if True in euclidean_dist:
-                    fst = euclidean_dist.index(True)
-                    name = self.known_name[fst]
-                else:
-                    continue
+            name = self._get_face_name(img_rep[i])
             self.face_name.insert(i, name)
 
         for i, rects in enumerate(img_detected):
             if self.face_name[i] in self.known_name:
-                cv2.rectangle(small_frame, (rects.left(), rects.top()), (rects.right(), rects.bottom()), (0, 255, 0), 2)
-                cv2.putText(small_frame, self.face_name[i], (rects.left(), rects.top()), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (0, 255, 0), 2, cv2.LINE_AA)
+                self._draw_rectangle(small_frame, rects, (0, 255, 0), self.face_name[i])
             else:
-                cv2.rectangle(small_frame, (rects.left(), rects.top()), (rects.right(), rects.bottom()), (255, 0, 0), 2)
-                cv2.putText(small_frame, self.face_name[i], (rects.left(), rects.top()), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                            (255, 0, 0), 2, cv2.LINE_AA)
+                self._draw_rectangle(small_frame, rects, (255, 0, 0), self.face_name[i])
             center_x = (rects.right() + rects.left()) / 2
             self.face_center.append(center_x)
 
@@ -204,17 +201,32 @@ class FaceRecog:
 
         return len(img_detected)
 
+    def _get_face_name(self, img_rep):
+        name = 'Face'
+        for j in range(len(self.known_face)):
+            euclidean_dist = list(np.linalg.norm(self.known_face - img_rep, axis=1) <= 0.62)
+            if True in euclidean_dist:
+                fst = euclidean_dist.index(True)
+                name = self.known_name[fst]
+            else:
+                continue
+        return name
+
+    def _draw_rectangle(self, frame, rects, color, name):
+        cv2.rectangle(frame, (rects.left(), rects.top()), (rects.right(), rects.bottom()), color, 2)
+        cv2.putText(frame, name, (rects.left(), rects.top()), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+
     def start(self, data, nome_main):
-        self._load_data()
+        self.load_data()
 
         small_frame = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
 
-        num_faces = self._recognize(small_frame)
+        num_faces = self.recognize(small_frame)
 
         name = ''
         center = 0.0
         if nome_main == '':
-            self._find_sit()
+            self.find_sit()
             print(self.center_place)
             self.recog = 1
         elif nome_main in self.face_name:
@@ -230,7 +242,7 @@ class FaceRecog:
     def handler(self, request):
         self.recog = 0
         while self.recog == 0:
-            self.image_sub = rospy.Subscriber(self.topic, Image, self._camera_callback)
+            self.image_sub = rospy.Subscriber(self.topic, Image, self.camera_callback)
             name, center, num, empty = self.start(self.cam_image, request.name)
             self.rate.sleep()
             return name, float(center), num, empty
