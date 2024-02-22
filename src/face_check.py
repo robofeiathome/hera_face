057137d912cf36bbeba9bffa27dcca1c6f9e20b7
cauansousa <cauanvinicius00@gmail.com>
2024-02-22 11:10:53 -0300

    refactor face_check

--- src/face_check.py
+++ src/face_check.py
@@ -1,42 +1,35 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Image
-from hera_face.srv import face_check
+from hera_face.srv import face_check, face_checkResponse
 import cv2
 import dlib
 from cv_bridge import CvBridge, CvBridgeError
 
-
 class FaceCheck:
     def __init__(self):
         self.bridge_object = CvBridge()
-        self.topic = "/zed_node/left_raw/image_raw_color"
-        self.check_cam_ready()
+        self.topic = rospy.get_param('~camera_topic')
         self.image_sub = rospy.Subscriber(self.topic, Image, self.camera_callback)
         self.service = rospy.Service('face_check', face_check, self.handler)
-        self.rate = rospy.Rate(3)
         rospy.loginfo("Finished FaceCheck Init process, ready to check")
 
-    def check_cam_ready(self):
-        self.cam_image = None
-        while self.cam_image is None and not rospy.is_shutdown():
-            try:
-                self.cam_image = rospy.wait_for_message(self.topic, Image, timeout=1.0)
-                rospy.logdebug(f"Current {self.topic} READY=>{str(self.cam_image)}")
-            except:
-                rospy.logerr(f"Current {self.topic} not ready yet, retrying.")
-
     def camera_callback(self, data):
+        """Callback function that receives camera images."""
         self.cam_image = data
 
-    def face_check(self, data):
+    def face_check(self, img):
+        """Checks if there are any faces in the given image."""
         try:
-            video_capture = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+            cv_image = self.bridge_object.imgmsg_to_cv2(img, desired_encoding="bgr8")
         except CvBridgeError as e:
-            print(e)
+            rospy.logerr(e)
+            return False
 
-        small_frame = cv2.resize(video_capture, (0, 0), fx=1, fy=1)
-        face_locations = dlib.get_frontal_face_detector()(small_frame, 1)
+        # No need to resize if we are processing with 1:1 scale
+        small_frame = cv2.resize(cv_image, (0, 0), fx=1, fy=1)
+        face_detector = dlib.get_frontal_face_detector()
+        face_locations = face_detector(small_frame, 1)
 
         if not face_locations:
             rospy.logwarn("No Faces found, please get closer...")
@@ -46,13 +39,13 @@ class FaceCheck:
         return True
 
     def handler(self, request):
-        while True:
-            if self.face_check(self.cam_image):
-                return True
-            self.rate.sleep()
+        """Service handler to perform face check."""
+        if self.cam_image is None:
+            rospy.logwarn("No image available from camera.")
+            return False
 
-        cv2.destroyAllWindows()
-
+        result = self.face_check(self.cam_image)
+        return result
 
 if __name__ == '__main__':
     rospy.init_node('face_check', log_level=rospy.INFO)
